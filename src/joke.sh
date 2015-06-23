4181c42893b37c847c5997f11b3504f78a562c71
xiaohu50 <xiaohu50@live.com>
2015-06-23 15:13:55 +0800

    version1.0

--- src/joke.sh
+++ src/joke.sh
@@ -1,8 +1,45 @@
-workpath=/home/xiaohu_v/github/joker/src
-pic_in=/home/xiaohu_v/github/joker/pic_in
-pic_out=/home/xiaohu_v/github/joker/pic_out
-tmpdir=/home/xiaohu_v/github/joker/tmpd
+checktag=$1
+index=$2
+cores=$3
+logpath=$4
+jokerpath=$5
+urlfile=$6
+suspiciousDir=$7
+spamDir=$8
+timetag=$9
 
-mkdir -p $tmpdir
-cd $workpath
-python2.7 deal.py $pic_in $pic_out $tmpdir
+pic_cache_left=${logpath}/pic_cache_left${index}
+pic_cache_right=${logpath}/pic_cache_right${index}
+tmpdir=${logpath}/tmp_cache${index}
+mkdir -p ${tmpdir}
+mkdir -p $pic_cache_left
+mkdir -p $pic_cache_right
+rm -rf ${tmpdir}/*
+
+echo $checktag, $index, $cores, $logpath, $jokerpath
+
+cd ${jokerpath}
+python geturl.py $index $cores $urlfile ${logpath}/url${index}
+
+if [ $checktag == "left" ]
+then
+	pic_write=${pic_cache_left}
+	pic_read=${pic_cache_right}
+else
+	pic_write=${pic_cache_right}
+	pic_read=${pic_cache_left}
+fi
+
+echo "write in ${pic_write}"
+echo "read in ${pic_read}"
+
+cd ${pic_write}
+rm -rf ./*
+wget -b -i ${logpath}/url${index} -o wget.log
+
+
+cd ${jokerpath}
+bad_uid=${logpath}/bad_uid${index}
+spam_dict=${jokerpath}/spam.dict
+python deal.py $pic_read $tmpdir $spam_dict $bad_uid ${suspiciousDir} ${spamDir}
+
